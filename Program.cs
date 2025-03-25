e1ce92664d607e71556aab9d2dee4bdd2118820c
GabrielBGB <gabrielprogramacao@outlook.com.br>
2025-03-25 19:28:48 -0300

    Implemantada versão 1

--- Program.cs
+++ Program.cs
@@ -2,9 +2,28 @@
 {
     internal class Program
     {
+
+        //  Versão 2: Exibir palavra oculta com traços
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("-----------------------");
+                Console.WriteLine("Jogo da Forca");
+                Console.WriteLine("-----------------------");
+
+                Console.WriteLine("Digite uma letra: ");
+                char chute = Console.ReadLine()[0]; //obtem aapenas um caractere do que o usuario digita
+
+                Console.WriteLine(chute);
+
+                Console.ReadLine();
+
+            }
+
+        } 
+
         }
     }
 }
