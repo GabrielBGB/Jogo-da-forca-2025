bd6bf2b58fa873d3697b8860403a2765e4967b4a
GabrielBGB <gabrielprogramacao@outlook.com.br>
2025-03-25 19:50:38 -0300

    implementada funcionalidade da dica da palavra

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace JogoDaForca.ConsoleApp1
+﻿using System.Runtime.InteropServices;
+
+namespace JogoDaForca.ConsoleApp1
 {
     internal class Program
     {
@@ -8,10 +10,25 @@
         {
             while (true)
             {
-                Console.Clear();
+
+                string palavraSecreta = "MELANCIA";
+
+                char[] letrasEncontradas = new char[palavraSecreta.Length];
+
+                for (int caractere = 0; caractere , letrasEncontradas.Length; caractere++) ;
+                {
+                    // acessar o array no indice 0
+                    letrasEncontradas[caractere] = '-';
+                }
+
+                string dicaDaPalavra = String.Join(" ", lettrasEncontradas);
+
+                    Console.Clear();
                 Console.WriteLine("-----------------------");
                 Console.WriteLine("Jogo da Forca");
                 Console.WriteLine("-----------------------");
+                Console.WriteLine("Palavra secreta: " + dicaDaPalavr);
+                Console.WriteLine("--------------------------------------");
 
                 Console.WriteLine("Digite uma letra: ");
                 char chute = Console.ReadLine()[0]; //obtem aapenas um caractere do que o usuario digita
